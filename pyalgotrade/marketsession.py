ff3f1968fa83039479ea70a06a5c81fe3d506e68
Massimo Fierro <massimo.fierro@ghrholdings.com>
2017-07-19 16:52:11 +0900

    Improved definition of market sessions following programs such as TradeStation and MultiCharts

--- pyalgotrade/marketsession.py
+++ pyalgotrade/marketsession.py
@@ -19,7 +19,38 @@
 """
 
 from datetime import time
+from enum import Enum
 import pytz
+
+
+class Weekdays(Enum):
+    Su = 0 Sun = 0 Sunday = 0
+    Mo = 1 Mon = 1 Monday = 1
+    Tu = 2 Tue = 2 Tuesday = 2
+    We = 3 Wed = 3 Wednesday = 3
+    Th = 4 Thu = 4 Thursday = 4
+    Fr = 5 Fri = 5 Friday = 5
+    Sa = 6 Sat = 6 Saturday = 6
+
+
+class WeekdayTime(object):
+    def __init__(self):
+        self.time = time(0, 0, 0)
+        self.weekday = Weekdays.Mo
+
+
+class DailySession(object):
+    def __init__(self, start, end):
+        if start is None or end is None:
+            raise ValueError('Start/End is None')
+        self.start = start
+        self.end = end
+        invertedWeekdays = end.weekday < start.weekday
+        invertedTimes = end.weekday <= start.weekday and end.time < start.time
+        if invertedWeekdays:
+            raise ValueError('End weekday is earlier than start weekday')
+        elif invertedTimes:
+            raise ValueError('End time is earlier than start time')
 
 
 # http://en.wikipedia.org/wiki/List_of_market_opening_times
@@ -38,59 +69,142 @@ class MarketSession(object):
     @classmethod
     def getOpeningTime(self):
         """Returns the datetime.time representing the opening time."""
-        return self.openingTime
-
-    @classmethod
-    def getOpeningTime(self):
-        """Returns the datetime.time representing the closing time."""
-        return self.closingTime
-
+        return self.sessions
 
 ######################################################################
 # US
 
+
 class NASDAQ(MarketSession):
     """NASDAQ market session."""
     timezone = pytz.timezone("US/Eastern")
+    sessions = [
+        DailySession(WeekdayTime(Weekdays.Mo, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Mo, time(16, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Tu, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Tu, time(16, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.We, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.We, time(16, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Th, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Th, time(16, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Fr, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Fr, time(16, tzinfo=timezone)))
+    ]
 
 
 class NYSE(MarketSession):
     """New York Stock Exchange market session."""
     timezone = pytz.timezone("US/Eastern")
-
-
-class USEquities(MarketSession):
-    """US Equities market session."""
-    timezone = pytz.timezone("US/Eastern")
+    sessions = [
+        DailySession(WeekdayTime(Weekdays.Mo, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Mo, time(16, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Tu, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Tu, time(16, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.We, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.We, time(16, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Th, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Th, time(16, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Fr, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Fr, time(16, tzinfo=timezone)))
+    ]
 
 
 class ICE(MarketSession):
-    timezone = pytz.timezone("US/Eastern")  # Fix
+    # https://www.theice.com/productguide/Search.shtml?tradingHours=
+    # Trading times for ICE can be computed either as London based or
+    # US Eastern (NY) based. Rounding all minutes to zero (sessions
+    # shorter by 10 minutes, except on Sunday).
+    timezone = pytz.timezone("US/Eastern")
+    sessions = [
+        DailySession(WeekdayTime(Weekdays.Su, time(17, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Mo, time(18, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Mo, time(19, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Tu, time(18, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Tu, time(19, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.We, time(18, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.We, time(19, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Th, time(18, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Th, time(19, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Fr, time(18, tzinfo=timezone)))
+    ]
 
 
 class CME(MarketSession):
+    # http://www.cmegroup.com/trading-hours.html#energy
+    # Trading times for CME can be computed either US Eastern or
+    # US Central based. Energy products are traded for the longest
+    # time (23hrs/day) whereas agricultural products have shorter trading
+    # sessions that will need to be specified in the products themselves.
     timezone = pytz.timezone("US/Central")  # Fix
+    sessions = [
+        DailySession(WeekdayTime(Weekdays.Su, time(17, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Mo, time(16, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Mo, time(17, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Tu, time(16, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Tu, time(17, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.We, time(16, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.We, time(17, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Th, time(16, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Th, time(17, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Fr, time(16, tzinfo=timezone)))
+    ]
 
 
 ######################################################################
 # South America
 
-class MERVAL(MarketSession):
+class BCBA(MarketSession):
     """Buenos Aires (Argentina) market session."""
     timezone = pytz.timezone("America/Argentina/Buenos_Aires")
+    sessions = [
+        DailySession(WeekdayTime(Weekdays.Mo, time(11, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Mo, time(17, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Tu, time(11, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Tu, time(17, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.We, time(11, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.We, time(17, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Th, time(11, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Th, time(17, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Fr, time(11, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Fr, time(17, tzinfo=timezone)))
+    ]
 
 
 class BOVESPA(MarketSession):
     """BOVESPA (Brazil) market session."""
     timezone = pytz.timezone("America/Sao_Paulo")
+    sessions = [
+        DailySession(WeekdayTime(Weekdays.Mo, time(10, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Mo, time(17, 30, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Tu, time(10, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Tu, time(17, 30, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.We, time(10, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.We, time(17, 30, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Th, time(10, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Th, time(17, 30, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Fr, time(10, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Fr, time(17, 30, tzinfo=timezone)))
+    ]
 
 
 ######################################################################
 # Europe
 
-class FTSE(MarketSession):
+class LSE(MarketSession):
     """ London Stock Exchange market session."""
     timezone = pytz.timezone("Europe/London")
+    sessions = [
+        DailySession(WeekdayTime(Weekdays.Mo, time(8, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Mo, time(16, 30, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Tu, time(8, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Tu, time(16, 30, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.We, time(8, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.We, time(16, 30, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Th, time(8, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Th, time(16, 30, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Fr, time(8, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Fr, time(16, 30, tzinfo=timezone)))
+    ]
 
 
 ######################################################################
@@ -99,10 +213,32 @@ class FTSE(MarketSession):
 class TSE(MarketSession):
     """Tokyo Stock Exchange market session."""
     timezone = pytz.timezone("Asia/Tokyo")
+    sessions = [
+        DailySession(WeekdayTime(Weekdays.Mo, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Mo, time(15, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Tu, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Tu, time(15, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.We, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.We, time(15, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Th, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Th, time(15, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Fr, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Fr, time(15, tzinfo=timezone)))
+    ]
 
 
 class KRX(MarketSession):
     """Korea Stock Exchange market session."""
     timezone = pytz.timezone("Asia/Seoul")
-    openingTime = time(9, 0, 0, 0, timezone)
-    clsoingTime = time(15, 45, 0, 0, timezone)
+    sessions = [
+        DailySession(WeekdayTime(Weekdays.Mo, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Mo, time(15, 30, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Tu, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Tu, time(15, 30, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.We, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.We, time(15, 30, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Th, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Th, time(15, 30, tzinfo=timezone))),
+        DailySession(WeekdayTime(Weekdays.Fr, time(9, tzinfo=timezone)),
+                     WeekdayTime(Weekdays.Fr, time(15, 30, tzinfo=timezone)))
+    ]
